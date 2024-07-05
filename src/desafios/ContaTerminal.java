99efb7e6c9eb0b5a642dda2fb921bd6ee81e9ee5
Nildo <nildo@webstack.com.br>
2024-07-05 00:55:42 -0300

    Finalizado exemplo para Conta Bancária Através Do Terminal/Console

---
+++ src/desafios/ContaTerminal.java
@@ -0,0 +1,33 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Numero da conta: ");
+        String numero = terminal.nextLine();
+
+        System.out.println("Número da agência: ");
+        String agencia = terminal.nextLine();
+
+        System.out.println("Nome cliente: ");
+        String titular = terminal.nextLine();
+
+        System.out.println("Saldo da conta: ");
+        double saldo = terminal.nextDouble();
+
+        System.out.println();
+        System.out.println("**************************");
+        System.out.println("Conta: " + numero);
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("**************************");
+
+
+
+        terminal.close();
+    }
+}
